cc40ab3ef44d261003b6aafbdada90ce770419c1
Jordan Gregory <jordan.gregory@paynearme.com>
2024-01-24 14:37:08 -0600

    add copilot back in

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,21 @@
 return {
   {
+    'zbirenbaum/copilot.lua',
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
+    'zbirenbaum/copilot-cmp',
+    after = { 'copilot.lua' },
+    config = function ()
+      require('copilot_cmp').setup()
+    end,
+  },
+  {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function ()
