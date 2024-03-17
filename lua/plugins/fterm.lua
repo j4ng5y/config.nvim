6561f8e18d547606f2dbe5d6b8a9c26d80b6217c
Jordan Gregory <jordan.gregory@paynearme.com>
2024-03-17 10:07:51 -0500

    remove fugative in favor of lazygit

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -4,6 +4,8 @@ return {
     config = function()
       vim.keymap.set('n', '<leader>tt', '<cmd>lua require("FTerm").toggle()<CR>', {desc = '[T]oggle [T]terminal'})
       vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><cmd>lua require("FTerm").toggle()<CR>', {desc = '[T]oggle [T]erminal'})
+      vim.keymap.set('n', '<leader>lg', '<cmd>lua require("FTerm").run("lazygit")<CR>', {desc = '[L]azy[G]it'})
+      vim.keymap.set('t', '<leader>lg', '<cmd>lua require("FTerm").close()<CR>', {desc = 'Close [L]azy[G]it'})
     end,
   }
 }
