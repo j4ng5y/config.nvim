88089026f472d104a579452fe5df060d060ef853
Jordan Gregory <jordan.gregory@paynearme.com>
2024-03-18 08:27:35 -0500

    update fterm

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -2,10 +2,35 @@ return {
   {
     'numToStr/FTerm.nvim',
     config = function()
-      vim.keymap.set('n', '<leader>tt', '<cmd>lua require("FTerm").toggle()<CR>', {desc = '[T]oggle [T]terminal'})
-      vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><cmd>lua require("FTerm").toggle()<CR>', {desc = '[T]oggle [T]erminal'})
-      vim.keymap.set('n', '<leader>lg', '<cmd>lua require("FTerm").run("lazygit")<CR>', {desc = '[L]azy[G]it'})
-      vim.keymap.set('t', '<leader>lg', '<cmd>lua require("FTerm").close()<CR>', {desc = 'Close [L]azy[G]it'})
+      local fterm = require('FTerm')
+      vim.keymap.set('n', '<leader>tt', function() fterm:toggle() end, {desc = '[T]oggle [T]terminal'})
+      vim.keymap.set('t', '<leader>tt', function() fterm:toggle() end, {desc = '[T]oggle [T]erminal'})
+    end,
+  },
+  {
+    'numToStr/FTerm.nvim',
+    name = 'FTerm.LazyGit.nvim',
+    config = function()
+      local fterm = require('FTerm')
+      local lazygit = fterm:new({
+        cmd = 'lazygit',
+      })
+
+      vim.keymap.set('n', '<leader>lg', function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
+      vim.keymap.set('t', '<leader>lg', function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
+    end,
+  },
+  {
+    'numToStr/FTerm.nvim',
+    name = 'FTerm.k9s.nvim',
+    config = function()
+      local fterm = require('FTerm')
+      local k9s = fterm:new({
+        cmd = 'k9s',
+      })
+
+      vim.keymap.set('n', '<leader>tk', function() k9s:toggle() end, {desc = '[T]oggle [K]9s'})
+      vim.keymap.set('t', '<leader>tk', function() k9s:toggle() end, {desc = '[T]oggle [K]9s'})
     end,
   }
 }
